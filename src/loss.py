40165859854fc5e1d1c23f12cb7dd3759157b0f1
Xenovortex <Leo.Nguyen@gmx.de>
2023-02-22 16:43:10 +0100

    transfer mmd multiscale loss function to src + unit testing

--- src/loss.py
+++ src/loss.py
@@ -9,6 +9,8 @@ from typing import Optional
 
 import torch
 
+from src.functionalities import get_device
+
 
 def l1_loss(
     pred: torch.Tensor,
@@ -45,4 +47,85 @@ def l2_loss(
         loss (torch.Tensor): L2-norm loss between predictions and labels
     """
     loss = torch.mean(torch.pow(pred - label, 2), dtype=dtype)
+    return loss
+
+
+def mmd_multiscale(
+    p_samples: torch.Tensor,
+    q_samples: torch.Tensor,
+    dtype: Optional[torch.dtype] = torch.float32,
+    device: Optional[str] = get_device(),
+) -> torch.Tensor:
+    """Multi-scale kernel Maximum Mean Discrepancy loss function.
+
+    Args:
+        p_samples (torch.Tensor): samples from distribution P
+        q_samples (torch.Tensor): samples from distribution Q
+        dtype (torch.dtype): return type. Defaults to torch.float32.
+        device (str): device. Defaults to get_device().
+
+    Returns:
+        loss (torch.Tensor): MMD loss between predictions and labels
+    """
+    p_samples, q_samples = p_samples.to(device), q_samples.to(device)
+
+    step_pp = (
+        (
+            torch.mm(p_samples, p_samples.t())
+            .diag()
+            .unsqueeze(0)
+            .expand_as(torch.mm(p_samples, p_samples.t()))
+        ).t()
+        + (
+            torch.mm(p_samples, p_samples.t())
+            .diag()
+            .unsqueeze(0)
+            .expand_as(torch.mm(p_samples, p_samples.t()))
+        )
+        - 2.0 * torch.mm(p_samples, p_samples.t())
+    )
+    step_qq = (
+        (
+            torch.mm(q_samples, q_samples.t())
+            .diag()
+            .unsqueeze(0)
+            .expand_as(torch.mm(q_samples, q_samples.t()))
+        ).t()
+        + (
+            torch.mm(q_samples, q_samples.t())
+            .diag()
+            .unsqueeze(0)
+            .expand_as(torch.mm(q_samples, q_samples.t()))
+        )
+        - 2.0 * torch.mm(q_samples, q_samples.t())
+    )
+    step_pq = (
+        (
+            torch.mm(p_samples, p_samples.t())
+            .diag()
+            .unsqueeze(0)
+            .expand_as(torch.mm(p_samples, p_samples.t()))
+        ).t()
+        + (
+            torch.mm(q_samples, q_samples.t())
+            .diag()
+            .unsqueeze(0)
+            .expand_as(torch.mm(q_samples, q_samples.t()))
+        )
+        - 2.0 * torch.mm(p_samples, q_samples.t())
+    )
+
+    sim_matrix_pp, sim_matrix_qq, sim_matrix_pq = (
+        torch.zeros(step_pp.shape).to(device),
+        torch.zeros(step_pp.shape).to(device),
+        torch.zeros(step_pp.shape).to(device),
+    )
+
+    for bandwidth in [0.2, 0.5, 0.9, 1.3]:
+        sim_matrix_pp += bandwidth**2 * (bandwidth**2 + step_pp) ** -1
+        sim_matrix_qq += bandwidth**2 * (bandwidth**2 + step_qq) ** -1
+        sim_matrix_pq += bandwidth**2 * (bandwidth**2 + step_pq) ** -1
+
+    loss = torch.mean(sim_matrix_pp + sim_matrix_qq - 2.0 * sim_matrix_pq, dtype=dtype)
+
     return loss
--- tests/test_loss.py
+++ tests/test_loss.py
@@ -1,6 +1,6 @@
 import torch
 
-from src.loss import l1_loss, l2_loss
+from src.loss import l1_loss, l2_loss, mmd_multiscale
 
 
 def test_l1_loss():
@@ -13,9 +13,18 @@ def test_l1_loss():
 
 
 def test_l2_loss():
-    pred = torch.randn(8, 10)
-    label = torch.randn(8, 10)
+    pred = torch.randn(4, 10)
+    label = torch.randn(4, 10)
     loss = l2_loss(pred, label)
+    assert isinstance(loss, torch.Tensor)
+    assert isinstance(loss.dtype, type(torch.float32))
+    assert loss >= 0
+
+
+def test_mmd_multiscale():
+    p_samples = torch.randn(4, 10)
+    q_samples = torch.randn(4, 10)
+    loss = mmd_multiscale(p_samples, q_samples)
     assert isinstance(loss, torch.Tensor)
     assert isinstance(loss.dtype, type(torch.float32))
     assert loss >= 0
