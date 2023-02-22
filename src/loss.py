a9580dfdea578a4ea7ba1fc3a8f683b6afadb483
Xenovortex <Leo.Nguyen@gmx.de>
2023-02-22 14:58:38 +0100

    transfer and unit test l2 norm loss function

--- src/loss.py
+++ src/loss.py
@@ -1,7 +1,8 @@
 """Loss Functions used for training.
 
 Functions:
-    l1_loss(torch.Tensor, torch.Tensor, torch.dtype) -> torch.Tensor
+    l1_loss(torch.Tensor, torch.Tensor, torch.dtype) -> torch.Tensor: L1 norm loss function
+    l2_loss(torch.Tensor, torch.Tensor, torch.dtype) -> torch.Tensor: L2 norm loss function
 """
 
 from typing import Optional
@@ -10,17 +11,38 @@ import torch
 
 
 def l1_loss(
-    pred: torch.Tensor, label: torch.Tensor, dtype: Optional[torch.dtype] = None
+    pred: torch.Tensor,
+    label: torch.Tensor,
+    dtype: Optional[torch.dtype] = torch.float32,
 ) -> torch.Tensor:
     """L1-norm loss function (least absolute deviations).
 
     Args:
         pred (torch.Tensor): batch of network predictions
         label (torch.Tensor): batch of labels
-        dtype (torch.dtype): return type
+        dtype (torch.dtype): return type. Defaults to torch.float32.
 
     Returns:
         loss (torch.Tensor): L1-norm loss between predictions and labels
     """
     loss = torch.mean(torch.abs(pred - label), dtype=dtype)
+    return loss
+
+
+def l2_loss(
+    pred: torch.Tensor,
+    label: torch.Tensor,
+    dtype: Optional[torch.dtype] = torch.float32,
+) -> torch.Tensor:
+    """L2-norm loss function (least squares error).
+
+    Args:
+        pred (torch.Tensor): batch of network predictions
+        label (torch.Tensor): batch of labels
+        dtype (torch.dtype): return type. Defaults to torch.float32.
+
+    Returns:
+        loss (torch.Tensor): L2-norm loss between predictions and labels
+    """
+    loss = torch.mean(torch.pow(pred - label, 2), dtype=dtype)
     return loss
--- tests/test_loss.py
+++ tests/test_loss.py
@@ -1,12 +1,21 @@
 import torch
 
-from src.loss import l1_loss
+from src.loss import l1_loss, l2_loss
 
 
 def test_l1_loss():
-    pred = torch.randn(32, 10)
-    label = torch.randn(32, 10)
-    loss = l1_loss(pred, label, torch.float)
-    print(type(loss))
+    pred = torch.randn(4, 10)
+    label = torch.randn(4, 10)
+    loss = l1_loss(pred, label)
     assert isinstance(loss, torch.Tensor)
+    assert isinstance(loss.dtype, type(torch.float32))
+    assert loss >= 0
+
+
+def test_l2_loss():
+    pred = torch.randn(8, 10)
+    label = torch.randn(8, 10)
+    loss = l2_loss(pred, label)
+    assert isinstance(loss, torch.Tensor)
+    assert isinstance(loss.dtype, type(torch.float32))
     assert loss >= 0
