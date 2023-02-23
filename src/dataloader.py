052dc5050b1562f0c7bfc41f5aa18d71632d267f
Xenovortex <Leo.Nguyen@gmx.de>
2023-02-23 15:20:02 +0100

    transfer cifar10 dataset loader with added unit testing

--- src/dataloader.py
+++ src/dataloader.py
@@ -21,3 +21,24 @@ def load_mnist() -> tuple[datasets.MNIST, datasets.MNIST]:
     testset = datasets.MNIST(save_path, train=False, download=True, transform=transform)
 
     return trainset, testset
+
+
+def load_cifar() -> tuple[datasets.CIFAR10, datasets.CIFAR10]:
+    """Load CIFAR10 dataset.
+
+    Returns:
+        trainset (datasets.CIFAR10): training dataset
+        testset (datasets.CIFAR10): test dataset
+    """
+    save_path = "./datasets/cifar10"
+
+    transform = transforms.Compose([transforms.ToTensor()])
+
+    trainset = datasets.CIFAR10(
+        save_path, train=True, download=True, transform=transform
+    )
+    testset = datasets.CIFAR10(
+        save_path, train=False, download=True, transform=transform
+    )
+
+    return trainset, testset
--- tests/test_dataloader.py
+++ tests/test_dataloader.py
@@ -1,7 +1,7 @@
 import torch
 from torchvision import datasets
 
-from src.dataloader import load_mnist
+from src.dataloader import load_cifar, load_mnist
 
 
 def test_load_mnist():
@@ -20,3 +20,21 @@ def test_load_mnist():
     assert isinstance(test_data.dtype, type(torch.float32))
     assert isinstance(test_label, int)
     assert test_data.size() == (1, 28, 28)
+
+
+def test_load_cifar():
+    trainset, testset = load_cifar()
+    train_data, train_label = trainset[0]
+    test_data, test_label = testset[0]
+    assert isinstance(trainset, datasets.CIFAR10)
+    assert isinstance(testset, datasets.CIFAR10)
+    assert len(trainset) == 50000
+    assert len(testset) == 10000
+    assert isinstance(train_data, torch.Tensor)
+    assert isinstance(train_data.dtype, type(torch.float32))
+    assert isinstance(train_label, int)
+    assert train_data.size() == (3, 32, 32)
+    assert isinstance(test_data, torch.Tensor)
+    assert isinstance(test_data.dtype, type(torch.float32))
+    assert isinstance(test_label, int)
+    assert test_data.size() == (3, 32, 32)
