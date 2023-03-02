37e9bc2be3809e272508ccebbfff256d113887a6
Xenovortex <Leo.Nguyen@gmx.de>
2023-03-02 16:15:31 +0100

    encapsulate loss functions into a class + adjust unit testing

--- src/loss.py
+++ src/loss.py
@@ -1,168 +1,179 @@
-"""Loss Functions used for training.
+"""Loss functionalities used for training.
 
-Functions:
-    l1_loss(Tensor, Tensor, dtype) -> Tensor: L1 norm loss function
-    l2_loss(Tensor, Tensor, dtype) -> Tensor: L2 norm loss function
-    mmd_multiscale(Tensor, Tensor, dtype, str) -> Tensor: MMD loss function
-    inn_loss(Tensor, Tensor, Tensor, dict, dtype) -> Tensor: INN Autoencoder loss function loss
+Classes:
+    LossTracker: compute and tracks loss values.
 """
 
-from typing import Optional
-
+import numpy as np
 import torch
 
 from src.functionalities import get_device
 
 
-def l1_loss(
-    pred: torch.Tensor,
-    label: torch.Tensor,
-    dtype: Optional[torch.dtype] = torch.float32,
-) -> torch.Tensor:
-    """L1-norm loss function (least absolute deviations).
+class LossTracker:
+    """Class for computing and tracking loss values.
 
-    Args:
-        pred (torch.Tensor): batch of network predictions
-        label (torch.Tensor): batch of labels
-        dtype (torch.dtype): return type. Defaults to torch.float32.
+    Attributes:
+        hyp_dict (dict): collection of hyperparameters
+        train_loss_dict (dict): training loss values logger
+        test_loss_dict (dict): test loss values logger
 
-    Returns:
-        loss (torch.Tensor): L1-norm loss between predictions and labels
+    Methods:
+        l1_loss(Tensor, Tensor) -> Tensor: L1 norm loss function
+        l2_loss(Tensor, Tensor) -> Tensor: L2 norm loss function
+        mmd_multiscale(Tensor, Tensor) -> Tensor: MMD loss function
+        inn_loss(Tensor, Tensor, Tensor) -> Tensor: INN Autoencoder loss function loss
     """
-    loss = torch.mean(torch.abs(pred - label), dtype=dtype)
-    return loss
 
+    def __init__(self, hyp_dict: dict) -> None:
+        """Initialize loss tracker.
 
-def l2_loss(
-    pred: torch.Tensor,
-    label: torch.Tensor,
-    dtype: Optional[torch.dtype] = torch.float32,
-) -> torch.Tensor:
-    """L2-norm loss function (least squares error).
+        Args:
+            hyp_dict (dict): collection of hyperparameters
+        """
+        self.hyp_dict = hyp_dict
+        self.train_loss = {
+            "total": np.zeros(hyp_dict["num_epoch"]),
+            "rec": np.zeros(hyp_dict["num_epoch"]),
+            "dist": np.zeros(hyp_dict["num_epoch"]),
+            "sparse": np.zeros(hyp_dict["num_epoch"]),
+        }
+        self.test_loss = {
+            "total": np.zeros(hyp_dict["num_epoch"]),
+            "rec": np.zeros(hyp_dict["num_epoch"]),
+            "dist": np.zeros(hyp_dict["num_epoch"]),
+            "sparse": np.zeros(hyp_dict["num_epoch"]),
+        }
 
-    Args:
-        pred (torch.Tensor): batch of network predictions
-        label (torch.Tensor): batch of labels
-        dtype (torch.dtype): return type. Defaults to torch.float32.
+    def l1_loss(self, pred: torch.Tensor, label: torch.Tensor) -> torch.Tensor:
+        """L1-norm loss function (least absolute deviations).
 
-    Returns:
-        loss (torch.Tensor): L2-norm loss between predictions and labels
-    """
-    loss = torch.mean(torch.pow(pred - label, 2), dtype=dtype)
-    return loss
+        Args:
+            pred (torch.Tensor): batch of network predictions
+            label (torch.Tensor): batch of labels
 
+        Returns:
+            loss (torch.Tensor): L1-norm loss between predictions and labels
+        """
+        loss = torch.mean(torch.abs(pred - label), dtype=self.hyp_dict["dtype"])
+        return loss
 
-def mmd_multiscale(
-    p_samples: torch.Tensor,
-    q_samples: torch.Tensor,
-    dtype: Optional[torch.dtype] = torch.float32,
-) -> torch.Tensor:
-    """Multi-scale kernel Maximum Mean Discrepancy loss function.
+    def l2_loss(self, pred: torch.Tensor, label: torch.Tensor) -> torch.Tensor:
+        """L2-norm loss function (least squares error).
 
-    Args:
-        p_samples (torch.Tensor): samples from distribution P
-        q_samples (torch.Tensor): samples from distribution Q
-        dtype (torch.dtype): return type. Defaults to torch.float32.
+        Args:
+            pred (torch.Tensor): batch of network predictions
+            label (torch.Tensor): batch of labels
 
-    Returns:
-        loss (torch.Tensor): MMD loss between predictions and labels
-    """
-    device = get_device()
-    p_samples, q_samples = p_samples.to(device), q_samples.to(device)
+        Returns:
+            loss (torch.Tensor): L2-norm loss between predictions and labels
+        """
+        loss = torch.mean(torch.pow(pred - label, 2), dtype=self.hyp_dict["dtype"])
+        return loss
 
-    step_pp = (
-        (
-            torch.mm(p_samples, p_samples.t())
-            .diag()
-            .unsqueeze(0)
-            .expand_as(torch.mm(p_samples, p_samples.t()))
-        ).t()
-        + (
-            torch.mm(p_samples, p_samples.t())
-            .diag()
-            .unsqueeze(0)
-            .expand_as(torch.mm(p_samples, p_samples.t()))
+    def mmd_multiscale(
+        self, p_samples: torch.Tensor, q_samples: torch.Tensor
+    ) -> torch.Tensor:
+        """Multi-scale kernel Maximum Mean Discrepancy loss function.
+
+        Args:
+            p_samples (torch.Tensor): samples from distribution P
+            q_samples (torch.Tensor): samples from distribution Q
+
+        Returns:
+            loss (torch.Tensor): MMD loss between predictions and labels
+        """
+        device = get_device()
+        p_samples, q_samples = p_samples.to(device), q_samples.to(device)
+
+        step_pp = (
+            (
+                torch.mm(p_samples, p_samples.t())
+                .diag()
+                .unsqueeze(0)
+                .expand_as(torch.mm(p_samples, p_samples.t()))
+            ).t()
+            + (
+                torch.mm(p_samples, p_samples.t())
+                .diag()
+                .unsqueeze(0)
+                .expand_as(torch.mm(p_samples, p_samples.t()))
+            )
+            - 2.0 * torch.mm(p_samples, p_samples.t())
         )
-        - 2.0 * torch.mm(p_samples, p_samples.t())
-    )
-    step_qq = (
-        (
-            torch.mm(q_samples, q_samples.t())
-            .diag()
-            .unsqueeze(0)
-            .expand_as(torch.mm(q_samples, q_samples.t()))
-        ).t()
-        + (
-            torch.mm(q_samples, q_samples.t())
-            .diag()
-            .unsqueeze(0)
-            .expand_as(torch.mm(q_samples, q_samples.t()))
+        step_qq = (
+            (
+                torch.mm(q_samples, q_samples.t())
+                .diag()
+                .unsqueeze(0)
+                .expand_as(torch.mm(q_samples, q_samples.t()))
+            ).t()
+            + (
+                torch.mm(q_samples, q_samples.t())
+                .diag()
+                .unsqueeze(0)
+                .expand_as(torch.mm(q_samples, q_samples.t()))
+            )
+            - 2.0 * torch.mm(q_samples, q_samples.t())
         )
-        - 2.0 * torch.mm(q_samples, q_samples.t())
-    )
-    step_pq = (
-        (
-            torch.mm(p_samples, p_samples.t())
-            .diag()
-            .unsqueeze(0)
-            .expand_as(torch.mm(p_samples, p_samples.t()))
-        ).t()
-        + (
-            torch.mm(q_samples, q_samples.t())
-            .diag()
-            .unsqueeze(0)
-            .expand_as(torch.mm(q_samples, q_samples.t()))
+        step_pq = (
+            (
+                torch.mm(p_samples, p_samples.t())
+                .diag()
+                .unsqueeze(0)
+                .expand_as(torch.mm(p_samples, p_samples.t()))
+            ).t()
+            + (
+                torch.mm(q_samples, q_samples.t())
+                .diag()
+                .unsqueeze(0)
+                .expand_as(torch.mm(q_samples, q_samples.t()))
+            )
+            - 2.0 * torch.mm(p_samples, q_samples.t())
         )
-        - 2.0 * torch.mm(p_samples, q_samples.t())
-    )
 
-    sim_matrix_pp, sim_matrix_qq, sim_matrix_pq = (
-        torch.zeros(step_pp.shape).to(device),
-        torch.zeros(step_pp.shape).to(device),
-        torch.zeros(step_pp.shape).to(device),
-    )
+        sim_matrix_pp, sim_matrix_qq, sim_matrix_pq = (
+            torch.zeros(step_pp.shape).to(device),
+            torch.zeros(step_pp.shape).to(device),
+            torch.zeros(step_pp.shape).to(device),
+        )
 
-    for bandwidth in [0.2, 0.5, 0.9, 1.3]:
-        sim_matrix_pp += bandwidth**2 * (bandwidth**2 + step_pp) ** -1
-        sim_matrix_qq += bandwidth**2 * (bandwidth**2 + step_qq) ** -1
-        sim_matrix_pq += bandwidth**2 * (bandwidth**2 + step_pq) ** -1
+        for bandwidth in [0.2, 0.5, 0.9, 1.3]:
+            sim_matrix_pp += bandwidth**2 * (bandwidth**2 + step_pp) ** -1
+            sim_matrix_qq += bandwidth**2 * (bandwidth**2 + step_qq) ** -1
+            sim_matrix_pq += bandwidth**2 * (bandwidth**2 + step_pq) ** -1
 
-    loss = torch.mean(sim_matrix_pp + sim_matrix_qq - 2.0 * sim_matrix_pq, dtype=dtype)
+        loss = torch.mean(
+            sim_matrix_pp + sim_matrix_qq - 2.0 * sim_matrix_pq,
+            dtype=self.hyp_dict["dtype"],
+        )
 
-    return loss
+        return loss
 
+    def inn_loss(
+        self, inp: torch.Tensor, rec: torch.Tensor, lat: torch.Tensor
+    ) -> list[torch.Tensor]:
+        """Compute INN Autoencoder loss.
 
-def inn_loss(
-    inp: torch.Tensor,
-    rec: torch.Tensor,
-    lat: torch.Tensor,
-    hyp_dict: dict,
-    dtype: Optional[torch.dtype] = torch.float32,
-) -> list[torch.Tensor]:
-    """Compute INN Autoencoder loss.
+        Args:
+            inp (torch.Tensor): input image
+            rec (torch.Tensor): reconstructed image
+            lat (torch.Tensor): latent vector
 
-    Args:
-        inp (torch.Tensor): input image
-        rec (torch.Tensor): reconstructed image
-        lat (torch.Tensor): latent vector
-        hyp_dict (dict): collection of hyperparameters (latent dimension and loss term coefficients)
-        dtype (torch.dtype): return type. Defaults to torch.float32.
+        Returns:
+            list[torch.Tensor]: total, reconstruction, distribution and sparse loss
+        """
+        base_dist = torch.normal(0, 1, size=(lat.size(0), self.hyp_dict["lat_dim"]))
 
-    Returns:
-        list[torch.Tensor]: total, reconstruction, distribution and sparse loss
-    """
-    base_dist = torch.normal(0, 1, size=(lat.size(0), hyp_dict["lat_dim"]))
+        l_rec = self.hyp_dict["a_rec"] * self.l1_loss(inp, rec)
+        l_dist = self.hyp_dict["a_dist"] * self.mmd_multiscale(
+            lat[:, : self.hyp_dict["lat_dim"]], base_dist
+        )
+        l_sparse = self.hyp_dict["a_sparse"] * self.l2_loss(
+            lat[:, self.hyp_dict["lat_dim"] :],
+            torch.zeros(lat.size(0), lat.size(1) - self.hyp_dict["lat_dim"]),
+        )
 
-    l_rec = hyp_dict["a_rec"] * l1_loss(inp, rec, dtype=dtype)
-    l_dist = hyp_dict["a_dist"] * mmd_multiscale(
-        lat[:, : hyp_dict["lat_dim"]], base_dist, dtype=dtype
-    )
-    l_sparse = hyp_dict["a_sparse"] * l2_loss(
-        lat[:, hyp_dict["lat_dim"] :],
-        torch.zeros(lat.size(0), lat.size(1) - hyp_dict["lat_dim"]),
-        dtype=dtype,
-    )
+        l_total = l_rec + l_dist + l_sparse
 
-    l_total = l_rec + l_dist + l_sparse
-
-    return [l_total, l_rec, l_dist, l_sparse]
+        return [l_total, l_rec, l_dist, l_sparse]
--- tests/test_loss.py
+++ tests/test_loss.py
@@ -1,46 +1,39 @@
 import torch
 
-from src.loss import inn_loss, l1_loss, l2_loss, mmd_multiscale
+from src.loss import LossTracker
 
 
-def test_l1_loss():
+def test_LossTracker():
+    hyp_dict = {
+        "num_epoch": 3,
+        "lat_dim": 5,
+        "a_rec": 1,
+        "a_dist": 1,
+        "a_sparse": 1,
+        "dtype": torch.float32,
+    }
+    loss_tracker = LossTracker(hyp_dict)
     pred = torch.randn(4, 10)
     label = torch.randn(4, 10)
-    loss = l1_loss(pred, label)
-    assert isinstance(loss, torch.Tensor)
-    assert isinstance(loss.dtype, type(torch.float32))
-    assert loss.dim() == 0
-    assert loss >= 0
-
-
-def test_l2_loss():
-    pred = torch.randn(4, 10)
-    label = torch.randn(4, 10)
-    loss = l2_loss(pred, label)
-    assert isinstance(loss, torch.Tensor)
-    assert isinstance(loss.dtype, type(torch.float32))
-    assert loss.dim() == 0
-    assert loss >= 0
-
-
-def test_mmd_multiscale():
-    p_samples = torch.randn(4, 10)
-    q_samples = torch.randn(4, 10)
-    loss = mmd_multiscale(p_samples, q_samples)
-    assert isinstance(loss, torch.Tensor)
-    assert isinstance(loss.dtype, type(torch.float32))
-    assert loss.dim() == 0
-    assert loss >= 0
-
-
-def test_inn_loss():
-    inp = torch.randn(4, 10)
-    rec = torch.randn(4, 10)
     lat = torch.randn(4, 10)
-    hyp_dict = {"lat_dim": 5, "a_rec": 1, "a_dist": 1, "a_sparse": 1}
-    loss = inn_loss(inp, rec, lat, hyp_dict)
-    assert isinstance(loss, list)
-    for item in loss:
+    l1_loss = loss_tracker.l1_loss(pred, label)
+    l2_loss = loss_tracker.l2_loss(pred, label)
+    mmd_loss = loss_tracker.mmd_multiscale(pred, label)
+    inn_loss = loss_tracker.inn_loss(pred, label, lat)
+    assert isinstance(l1_loss, torch.Tensor)
+    assert isinstance(l1_loss.dtype, type(torch.float32))
+    assert l1_loss.dim() == 0
+    assert l1_loss >= 0
+    assert isinstance(l2_loss, torch.Tensor)
+    assert isinstance(l2_loss.dtype, type(torch.float32))
+    assert l2_loss.dim() == 0
+    assert l2_loss >= 0
+    assert isinstance(mmd_loss, torch.Tensor)
+    assert isinstance(mmd_loss.dtype, type(torch.float32))
+    assert mmd_loss.dim() == 0
+    assert mmd_loss >= 0
+    assert isinstance(inn_loss, list)
+    for item in inn_loss:
         assert isinstance(item, torch.Tensor)
         assert isinstance(item.dtype, type(torch.float32))
         assert item.dim() == 0
